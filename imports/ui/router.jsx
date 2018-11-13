243f75f28a783d5aba2d7742a41b383177dd2e03
Fred <john.bashh@gmail.com>
2018-11-13 19:20:05 +0100

    [Helmet] Add helmet

--- imports/ui/router.jsx
+++ imports/ui/router.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch, withRouter } from 'react-router';
+// PAGES
 import Home from './pages/home';
 import About from './pages/about';
+// APP
 import App from './app';
 
-export const Router = () =>
+export const Router = withRouter(({ location }) => {
+  return (
     <App>
-      <Route exact path="/" render={(props) => <Home {...props} />} />
-      <Route path="/about" render={(props) => <About {...props} />} />
-    </App>;
+      <Switch location={location}>
+        <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Route path="/about" render={(props) => <About {...props} />} />
+      </Switch>
+    </App>
+  );
+});
